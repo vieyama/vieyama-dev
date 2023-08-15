fa0e2f3e5c6d6a5aca6ad04bcf07a2d7aa09e44f
yoviefp <yoviefp@gmail.com>
2023-08-15 13:14:39 +0700

    fix: fix failed build by webpack in docker

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     webpack: (config, { isServer }) => {
         config.resolve.alias['~'] = path.join(__dirname, 'src');
         return config;
