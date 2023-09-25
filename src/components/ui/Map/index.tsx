c1e18c0a916e273562531216f8774b9775ee5b8d
yoviefp <yoviefp@gmail.com>
2023-09-25 07:21:20 +0700

    fix: update maps

--- src/components/ui/Map/index.tsx
+++ src/components/ui/Map/index.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import type {SetStateAction} from "react";
 import {useMemo, useState} from "react";
 
 import {GoogleMap, MarkerF, useLoadScript} from "@react-google-maps/api";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
 
 import type {Libraries} from "@react-google-maps/api";
 
@@ -15,13 +22,19 @@ const GoogleMaps = ({
   onChangeLatLong?: (latlong: LatlongType) => void;
   allowToChangePointing?: boolean;
 }) => {
-  const [latLong, setLatLong] = useState(latlong);
+  const [latLongDefault, setLatLongDetault] = useState(latlong);
+  const [latLongChanges, setLatLongChanges] = useState(latLongDefault);
 
   const libraries = useMemo(() => ["places"], []);
 
-  const mapCenters = useMemo(
-    () => ({lat: latLong.lat, lng: latLong.long}),
-    [latLong],
+  const mapCentersDefault = useMemo(
+    () => ({lat: latLongDefault.lat, lng: latLongDefault.long}),
+    [latLongDefault],
+  );
+
+  const mapCentersChanges = useMemo(
+    () => ({lat: latLongChanges.lat, lng: latLongChanges.long}),
+    [latLongChanges],
   );
 
   const mapOptions = useMemo<google.maps.MapOptions>(
@@ -38,27 +51,31 @@ const GoogleMaps = ({
     libraries: libraries as Libraries,
   });
 
-  // const [address, setAddress] = useState("");
-  // const handleChangeAddress = (newAddress: SetStateAction<string>) => {
-  //   setAddress(newAddress);
-  // };
-  // const handleSelectAddress = (newAddress: SetStateAction<string>) => {
-  //   setAddress(newAddress);
-  //   geocodeByAddress(newAddress as string)
-  //     .then((results) => getLatLng(results[0]))
-  //     .then((result) => {
-  //       setLatLong({
-  //         lat: result?.lat,
-  //         long: result?.lng,
-  //       });
+  const [address, setAddress] = useState("");
+  const handleChangeAddress = (newAddress: SetStateAction<string>) => {
+    setAddress(newAddress);
+  };
+  const handleSelectAddress = (newAddress: SetStateAction<string>) => {
+    setAddress(newAddress);
+    geocodeByAddress(newAddress as string)
+      .then((results) => getLatLng(results[0]))
+      .then((result) => {
+        setLatLongDetault({
+          lat: result?.lat,
+          long: result?.lng,
+        });
+        setLatLongChanges({
+          lat: result?.lat,
+          long: result?.lng,
+        });
 
-  //       onChangeLatLong?.({
-  //         lat: result?.lat,
-  //         long: result?.lng,
-  //       });
-  //     })
-  //     .catch((error) => console.error("Error", error));
-  // };
+        onChangeLatLong?.({
+          lat: result?.lat,
+          long: result?.lng,
+        });
+      })
+      .catch((error) => console.error("Error", error));
+  };
 
   if (!isLoaded) {
     return <p>Loading...</p>;
@@ -66,7 +83,7 @@ const GoogleMaps = ({
 
   return (
     <div className="w-full">
-      {/* {allowToChangePointing && isLoaded ? (
+      {allowToChangePointing && isLoaded ? (
         <>
           <PlacesAutocomplete
             value={address}
@@ -123,14 +140,14 @@ const GoogleMaps = ({
             )}
           </PlacesAutocomplete>
         </>
-      ) : null} */}
+      ) : null}
       <GoogleMap
         options={mapOptions}
         zoom={14}
-        center={mapCenters}
+        center={mapCentersDefault}
         onClick={(e) => {
           if (allowToChangePointing) {
-            setLatLong({
+            setLatLongChanges({
               lat: e.latLng?.lat() as number,
               long: e.latLng?.lng() as number,
             });
@@ -142,7 +159,7 @@ const GoogleMaps = ({
         }}
         mapTypeId={google.maps.MapTypeId.ROADMAP}
         mapContainerStyle={{width: "100%", height: "240px"}}>
-        <MarkerF position={mapCenters} />
+        <MarkerF position={mapCentersChanges} />
       </GoogleMap>
     </div>
   );
