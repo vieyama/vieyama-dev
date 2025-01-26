33b7a44d48fbe51af36c9eb295988de88dfe6a82
Yovie Fesya Pratama <yoviefp@gmail.com>
2025-01-26 19:40:38 +0700

    fix: fixing error build

--- src/app/section/about-me.tsx
+++ src/app/section/about-me.tsx
@@ -15,7 +15,7 @@ const AboutMe: React.FC<{}> = () => {
         className="shadow-2xl rounded-xl"
       />
       <p className="w-5/6 mt-5 text-base text-justify">
-        Hi :) 👋 I`'m Yovie Fesya Pratama, A creative software developer with 5 years of experience. Skilled and experienced in designing, developing, testing and deploying websites using up-to-date technologies. Have professionally used JavaScript, TypeScript, ReactJS, NextJS, NodeJS, GraphQL, HTML and CSS. Eager to face more challenges regarding ways to optimize user experience.
+        Hi :) 👋 I&lsquo;m Yovie Fesya Pratama, A creative software developer with 5 years of experience. Skilled and experienced in designing, developing, testing and deploying websites using up-to-date technologies. Have professionally used JavaScript, TypeScript, ReactJS, NextJS, Angular, NestJS, NodeJS, Laravel, WordPress, GraphQL, HTML and CSS. Eager to face more challenges regarding ways to optimize user experience.
       </p>
     </div>
   );
